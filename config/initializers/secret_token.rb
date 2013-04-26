1cc93cab74595ecc05ea2a74f491154282988ada
lighthouselc <korovin@lighthouse.lc>
2013-04-26 19:03:45 +0400

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lighthouse::Application.config.secret_token = 'e58ecfda418b9cb89bd5393b19cc7c3fe6b1e422a183260ec18d1ca85755d379ee7aa44aaf9398f06e3f9bd264f1a3fae925676ad6bf72e3fcf94c19bcee7725'
